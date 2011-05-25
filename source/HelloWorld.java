94d5b4b97a6954eaf20f1176d51d9549842468a0
Connie Li <Connie.Li@nasa.gov>
2011-05-25 13:46:38 -0400

    Signed-off-by: Connie Li <Connie.Li@nasa.gov>

---
+++ source/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld
+{
+    /**
+     * Implement the appplication
+     * @param args arguments
+     */
+    public static void main (String[] args)
+    {
+        System.out.println ("Hello World!");
+    }
+}
